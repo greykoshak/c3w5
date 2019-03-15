982abf299cde04aebccd3c53b0841788f3a4d354
Igor Agarkov <igor.agarkov.007@gmail.com>
2019-03-15 16:19:33 +0200

    c3w5 init

---
+++ coursera_forms/formdummy/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+from django.views import View
+
+class FormDummyView(View):
+    def get(self, request):
+        # from pdb import set_trace; set_trace()
+        return render(request, "form.html", {})
+
+    def post(self, request):
+        text = request.POST.get('text')
+        grade = request.POST.get('grade')
+        image = request.FILES.get('image')
+        content = image.read()
+        context = {
+            'text': text,
+            'grade': grade,
+            'content': content
+        }
+        return render(request, "form.html", context)
